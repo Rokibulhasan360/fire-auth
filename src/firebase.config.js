d21fae35429c5b0b5254cdd3aa1c9af9b92b02f9
Rokibulhasan360 <rokibulhasan360@gmail.com>
2020-04-23 15:50:43 +0600

    finished authentication

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTBab7vJ2VfzjQaTxi0DHy8Io6TcVR9Ko",
+    authDomain: "ema-john-simple-7a64c.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-7a64c.firebaseio.com",
+    projectId: "ema-john-simple-7a64c",
+    storageBucket: "ema-john-simple-7a64c.appspot.com",
+    messagingSenderId: "857630494041",
+    appId: "1:857630494041:web:325ab51f32334949a01048"
+  };
+  export default firebaseConfig; 
